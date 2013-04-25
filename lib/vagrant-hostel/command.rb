47178776143ab302bc58dd538da027b0a461307d
Paul Garner <ego@anentropic.com>
2013-04-25 14:36:13 +0100

    more notes, no progress

--- lib/vagrant-hostel/command.rb
+++ lib/vagrant-hostel/command.rb
@@ -1,25 +1,37 @@
 module VagrantHostel
   class Command < Vagrant.plugin(2, :command)
-	  def execute
+ 	  def execute
 			options = {}
 			options[:force] = false
 
 			opts = OptionParser.new do |o|
-			  o.banner = "Usage: vagrant hostel [vm-name] [no. to replicate]"
+			  o.banner = "Usage: vagrant hostel [vm-name] [stock (no. of instances)]"
 			  o.separator ""
 			end
 
 			# Parse the options
 			argv = parse_options(opts)
 
-	    puts "Hostel: #{argv}"
+	    puts "Hostel GIT: #{argv}"
 	    vm_name = argv[0]
-	    multiple = argv[1]
+	    stock = argv[1]
 	    # TODO:
 	    # raise exceptions if these don't exist
 
-	    puts ">> #{@env.instance_variable_get(:@config_global).vm.instance_variable_get(:@__defined_vms)}"
-	    puts ">> #{@env.instance_variable_get(:@config_global).vm.instance_variable_get(:@__defined_vm_keys)}"
+	    global_config = @env.instance_variable_get(:@config_global)
+
+	    puts "> #{global_config.hostel.instance_variables.sort}"
+	    puts ">> #{global_config.vm.defined_vm_keys}"
+	    puts ">>> #{global_config.hostel.defined_vms}"
+	    puts ">>>> #{@env.machine_names}"
+
+	   	global_config.hostel.defined_vm_keys.each do |vm_name|
+	   		puts "#{vm_name} > #{global_config.hostel.defined_vms[vm_name].options}"
+	   	end
+
+	   	# do we accept `stock` via Vagrantfile config or just via command?
+	   	# if from command we need to persist stock value using something like
+	   	# StateFile (json file) from plugins/commands/plugin
 
 		  mw = ::Vagrant::Action::Builder.new.tap do |b|
         # we need to expand the VM definitions in our HostelVMConfig
@@ -35,6 +47,7 @@ module VagrantHostel
 	      #runner.run(mw, :machine=>vm)
 		  end
 
+		  # return status:
 	    0
 		end
   end
--- lib/vagrant-hostel/config.rb
+++ lib/vagrant-hostel/config.rb
@@ -1,4 +1,16 @@
 module VagrantHostel
   class HostelVMConfig < VagrantPlugins::Kernel_V2::VMConfig
+    def initialize
+      super
+      @__stock = 1
+    end
+
+    def stock= val
+      @__stock = val
+    end
+
+    def stock
+      @__stock
+    end
   end
 end
